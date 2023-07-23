31ff45d5b0432fc0934938c365fcb4c7454282dc
Harsha <sriharshaboina1998@gmail.com>
2023-07-23 14:59:42 +0000

    first commit

---
+++ code.java
@@ -0,0 +1,73 @@
+import java.util.Scanner;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Simple Calculator");
+        System.out.println("------------------");
+        System.out.println("Operations:");
+        System.out.println("1. Addition (+)");
+        System.out.println("2. Subtraction (-)");
+        System.out.println("3. Multiplication (*)");
+        System.out.println("4. Division (/)");
+        System.out.println("5. Exit");
+
+        while (true) {
+            System.out.print("Enter the operation number: ");
+            int choice = scanner.nextInt();
+
+            if (choice == 5) {
+                System.out.println("Calculator exiting...");
+                break;
+            }
+
+            System.out.print("Enter the first number: ");
+            double num1 = scanner.nextDouble();
+
+            System.out.print("Enter the second number: ");
+            double num2 = scanner.nextDouble();
+
+            switch (choice) {
+                case 1:
+                    System.out.println("Result: " + add(num1, num2));
+                    break;
+                case 2:
+                    System.out.println("Result: " + subtract(num1, num2));
+                    break;
+                case 3:
+                    System.out.println("Result: " + multiply(num1, num2));
+                    break;
+                case 4:
+                    if (num2 == 0) {
+                        System.out.println("Error: Division by zero is not allowed.");
+                    } else {
+                        System.out.println("Result: " + divide(num1, num2));
+                    }
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+        }
+
+        scanner.close();
+    }
+
+    public static double add(double a, double b) {
+        return a + b;
+    }
+
+    public static double subtract(double a, double b) {
+        return a - b;
+    }
+
+    public static double multiply(double a, double b) {
+        return a * b;
+    }
+
+    public static double divide(double a, double b) {
+        return a / b;
+    }
+}
+
